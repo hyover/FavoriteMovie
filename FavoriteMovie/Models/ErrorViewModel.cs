94463eda247b5f89b8f318836ef113424dccdaa3
hyover <vf.hyover@gmail.com>
2023-12-04 09:12:46 +0100

    Ajoutez des fichiers projet.

---
+++ FavoriteMovie/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FavoriteMovie.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
