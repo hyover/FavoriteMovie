794cb95edb48a7e903cf17a9c6210acd33bf253a
hyover <vf.hyover@gmail.com>
2023-12-04 10:11:11 +0100

    Add MediaGenre Model in Models Folder
    Add Media Model with this properties : Id, Name, Medias, CreatedAt, UpdatedAt

--- FavoriteMovie/Models/MediaGenre.cs
+++ FavoriteMovie/Models/MediaGenre.cs
@@ -4,5 +4,26 @@ namespace FavoriteMovie.Models
 {
     public class MediaGenre
     {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime CreatedAt { get; set; }
+
+        [DataType(DataType.Date)]
+        public DateTime UpdatedAt { get; set; }
+        public virtual ICollection<Media> Medias { get; set; }
+
+        // Constructor for CreatedAt
+        public MediaGenre()
+        {
+            CreatedAt = DateTime.Now;
+        }
+
+        // Function call before add or update an entity
+        public void BeforeSaveChanges()
+        {
+            UpdatedAt = DateTime.Now;
+        }
     }
 }
