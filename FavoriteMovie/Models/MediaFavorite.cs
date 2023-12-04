775b580de1c9d7e97b4f551b3ada06175f1febb4
hyover <vf.hyover@gmail.com>
2023-12-04 10:45:09 +0100

    Add MediaFavorite Model in Models Folder
    Add MediaFavorite Model with this properties : Id, Media, User

---
+++ FavoriteMovie/Models/MediaFavorite.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace FavoriteMovie.Models
+{
+    public class MediaFavorite
+    {
+        // PK
+        public int Id { get; set; }
+
+        // FK
+        public virtual IdentityUser User { get; set; }
+        public virtual Media Media { get; set; }
+    }
+}
